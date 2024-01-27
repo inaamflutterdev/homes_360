0536d6ef87b453a8675417094228fcf167abde40
Inaam Ul Haq <inaamulhaq7847@gmail.com>
2024-01-27 22:05:11 +0500

    Updates

--- lib/presentation/select_virtual_app_screen/models/select_virtual_app_model.dart
+++
@@ -1,3 +0,0 @@
-import '../../../core/app_export.dart';import 'selectvirtualapp_item_model.dart';class SelectVirtualAppModel {List<SelectvirtualappItemModel> selectvirtualappItemList = [SelectvirtualappItemModel(whatsapp:ImageConstant.imgWhatsapp,whatsapp1: "Whatsapp",recommend: "Recommend",check:ImageConstant.imgCheck)];
-
- }
