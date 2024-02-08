c19470ca31c76a75fa2dd7189f17c15e5ab70849
Inaam Ul Haq <inaamulhaq7847@gmail.com>
2024-02-09 02:04:08 +0500

    Updates

--- lib/presentation/add_new_property_home_facts_screen/models/add_new_property_home_facts_model.dart
+++
@@ -1,3 +0,0 @@
-import 'package:homes360/data/models/selectionPopupModel/selection_popup_model.dart';import '../../../core/app_export.dart';class AddNewPropertyHomeFactsModel {List<SelectionPopupModel> dropdownItemList = [SelectionPopupModel(id:1,title:"Item One",isSelected:true,),SelectionPopupModel(id:2,title:"Item Two",),SelectionPopupModel(id:3,title:"Item Three",)];
-
- }
--- lib/presentation/add_new_property_select_amenities_screen/models/add_new_property_select_amenities_model.dart
+++
@@ -1,6 +0,0 @@
-import 'options_item_model.dart';
-
-class AddNewPropertySelectAmenitiesModel {
-  List<OptionsItemModel> optionsItemList =
-      List.generate(8, (index) => OptionsItemModel());
-}
